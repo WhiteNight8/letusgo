3ba13193917716b155ccf83b02f5ad660bdb9ba5
Xu <whitenight.xiaxu@gmail.com>
2025-12-23 15:46:22 +0800

    create database connection pool

---
+++ internal/models/snippets.go
@@ -0,0 +1,30 @@
+package models
+
+import (
+	"database/sql"
+	"time"
+)
+
+type Snippet struct {
+	ID      int
+	Title   string
+	Content string
+	Created time.Time
+	Expires time.Time
+}
+
+type SnippetsModel struct {
+	db *sql.DB
+}
+
+func (m *SnippetsModel) Insert(title, content string, expires int) (int, error) {
+	return 0, nil
+}
+
+func (m *SnippetsModel) Get(id int) (*Snippet, error) {
+	return nil, nil
+}
+
+func (m *SnippetsModel) Latest() ([]*Snippet, error) {
+	return nil, nil
+}

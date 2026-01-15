6a529f41e1c99a5da6de5e1a0d3b2ab50b018316
Xu <whitenight.xiaxu@gmail.com>
2026-01-15 15:32:49 +0800

    custom template functions

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -3,6 +3,7 @@ package main
 import (
 	"html/template"
 	"path/filepath"
+	"time"
 
 	"letgo.snippetbox/internal/models"
 )
@@ -11,6 +12,14 @@ type templateData struct {
 	CurrentYear int
 	Snippet     *models.Snippet
 	Snippets    []*models.Snippet
+}
+
+func humanDate(t time.Time) string {
+	return t.Format("02 Jan 2006 at 15:04")
+}
+
+var functions = template.FuncMap{
+	"humanDate": humanDate,
 }
 
 func newTemplateCache() (map[string]*template.Template, error) {
@@ -24,7 +33,7 @@ func newTemplateCache() (map[string]*template.Template, error) {
 	for _, page := range page {
 		name := filepath.Base(page)
 
-		ts, err := template.ParseFiles(page, "./ui/html/pages/base.html")
+		ts, err := template.New(name).Funcs(functions).ParseFiles(page, "./ui/html/pages/base.html")
 		if err != nil {
 			return nil, err
 		}
